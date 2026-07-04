52e14bedc1f138d2623dc8de616f6f8983154d1e
mahendra <mahendrapra0077@gmail.com>
2026-07-05 04:56:23 +0530

    refactor: register all feature slices in redux store config

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import authReducer from "@/store/authSlice.js";
+import reviewsReducer from "@/store/reviewsSlice.js";
 import roomsReducer from "@/store/roomsSlice.js";
+import messagesReducer from "@/store/messagesSlice.js";
+import bookingReducer from "@/store/bookingSlice.js";
+import notificationsReducer from "@/store/notificationsSlice.js";
+import profileReducer from "@/store/profileSlice.js";
+import mapReducer from "@/store/mapSlice.js";
+import uiReducer from "@/store/uiSlice.js";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     rooms: roomsReducer,
+    reviews: reviewsReducer,
+    messages: messagesReducer,
+    booking: bookingReducer,
+    notifications: notificationsReducer,
+    profile: profileReducer,
+    map: mapReducer,
+    ui: uiReducer,
   },
 });
